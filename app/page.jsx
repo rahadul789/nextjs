8ea4c5f5b4ccda63578006a88a3324200dae670a
rahadul789 <haqrahadul@gmail.com>
2025-05-19 20:27:11 +0600

    simple data fetching

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,20 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
+
   return (
     <div className="">
       <h1>Welcome to Next.js!</h1>
-      <p>This is a simple example of a Next.js application.</p>
-      <Button>React</Button>
       <Link href="/dashboard">Dashboard</Link>
+
+      <div className=" mt-10">
+        <h1 className=" text-3xl font-semibold mb-4"> Data Featching</h1>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </div>
     </div>
   );
 }
